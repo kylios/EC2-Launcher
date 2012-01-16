849256aa082c0eb0fc65a773861a7084ffa2b141
kyle racette <kracette@gmail.com>
2012-01-15 23:42:41 -0500

    added a line

--- ec2.py
+++ ec2.py
@@ -1,5 +1,8 @@
 #!/usr/bin/env python2
 
+
+
+# Kyle was here!!!!
 # Author: Kyle Racette (kracette (at) gmail (dot) com)
 #
 #   This program is free software: you can redistribute it and/or modify
